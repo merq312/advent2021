c494df9a395ae1130784f80f473294dd5066d5eb
Chamila Gunasena <merc312@gmail.com>
2021-12-15 15:05:49 -0500

    advent 2

---
+++ advent-2/second.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+const data = fs
+  .readFileSync("data", { encoding: "utf-8", flag: "r" })
+  .trim()
+  .split("\n");
+
+let horz = 0;
+let depth = 0;
+let aim = 0;
+
+for (let ea of data) {
+  const cmd = ea.split(" ");
+  cmd[1] = parseFloat(cmd[1]);
+  switch (cmd[0]) {
+    case "forward":
+      horz += cmd[1];
+      depth += cmd[1] * aim;
+      break;
+    case "up":
+      aim -= cmd[1];
+      break;
+    case "down":
+      aim += cmd[1];
+      break;
+  }
+}
+
+console.log(horz, depth);
+console.log(horz * depth);
