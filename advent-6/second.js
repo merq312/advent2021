3c8499f9784b81435ae0d8dcb8b035af30c92124
merq312 <merc312@gmail.com>
2021-12-16 13:58:21 -0500

    day 6 part 2

--- advent-6/second.js
+++ advent-6/second.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 
 let data = fs
-  .readFileSync('test-data', { encoding: 'utf-8', flag: 'r' })
+  .readFileSync('data', { encoding: 'utf-8', flag: 'r' })
   .split(',')
   .map(parseFloat);
 
-for (let i = 0; i < 256; i++) {
-  let count = 0;
-  console.log(i);
-  data = data.map((ea) => {
-    if (ea === 0) {
-      ++count;
-      return 6;
-    } else {
-      return ea - 1;
-    }
-  });
-  data.push(...new Array(count).fill(8));
+const fish = new Array(9).fill(0)
+for (let ea of data) {
+  fish[ea]++
 }
-console.log(data.length);
+
+console.log(fish)
+for (let i = 0; i < 256; i++) {
+  const oldFish = fish.shift()
+  fish[6] += oldFish
+  fish[8] = oldFish
+  console.log(fish)
+}
+
+console.log(fish.reduce((previousValue, currentValue) => previousValue+currentValue))
