095c64b400c2a47bda625dfce04d229348c18f24
merq312 <merc312@gmail.com>
2022-01-20 00:57:03 -0500

    12 part 1

--- advent-12/main.cpp
+++ advent-12/main.cpp
@@ -6,9 +6,17 @@
 
 std::vector<std::string> getData(const std::string &);
 
-int main() {
-    auto data = getData("test-data");
+void first(std::vector<std::string> &data);
 
+int main() {
+    auto data = getData("data");
+
+    first(data);
+
+    return 0;
+}
+
+void first(std::vector<std::string> &data) {
     auto nodes = std::vector<std::pair<std::string, std::string>>();
 
     for (const auto &node: data) {
@@ -20,14 +28,7 @@ int main() {
 
     auto paths = std::vector<std::vector<std::string>>();
 
-    // FUNC node
-    // get all node connections
-    // get first non path node (IF not caps), push node to path
-    // IF node is end, push path to paths, return
-    // IF no valid nodes, return?
-    // CALL FUNC with node
-
-    auto getConnections = [&](const std::string &node) {
+    auto getConnections = [&nodes](const std::string &node) {
         auto conns = std::vector<std::string>();
         for (const auto &n: nodes) {
             if (node == n.first) {
@@ -40,36 +41,33 @@ int main() {
         return conns;
     };
 
-    std::function<void(std::string, std::vector<std::string>)> func =
-            [&](const std::string &node, std::vector<std::string> path) {
+    std::function<void(std::string, std::vector<std::string>)> findPaths =
+            [&findPaths, &paths, &getConnections](const std::string &node, std::vector<std::string> path) {
                 auto conns = getConnections(node);
                 for (auto conn: conns) {
                     auto conn_lower = conn;
                     std::transform(conn.begin(), conn.end(), conn_lower.begin(),
-                                   [](unsigned char c) { return std::tolower(c); });
+                                   [](unsigned char c) { return tolower(c); });
                     if (conn != conn_lower || std::find(path.begin(), path.end(), conn) == std::end(path)) {
                         path.emplace_back(conn);
                         if (conn == "end") {
                             paths.push_back(path);
-                            return;
+                            path.pop_back();
+                        } else {
+                            findPaths(conn, path);
+                            path.pop_back();
                         }
-                        if (conn.empty()) {
-                            return;
-                        }
-                        func(conn, path);
                     }
                 }
             };
 
-    // construct a path
     auto path = std::vector<std::string>();
-    // push start to path
+
     path.emplace_back("start");
 
-    // call FUNC start
-    func("start", path);
+    findPaths("start", path);
 
-    return 0;
+    std::cout << paths.size() << '\n';
 }
 
 std::vector<std::string> getData(const std::string &filename) {
